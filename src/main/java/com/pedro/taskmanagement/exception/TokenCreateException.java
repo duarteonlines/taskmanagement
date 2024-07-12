ef83c779fa5b518a4ad4808fa81beabfc5aa692c
Pedro Oliveira <168007803+oliveiraordep@users.noreply.github.com>
2024-07-12 09:15:02 -0300

    Revert "Feature/add users roles"

--- src/main/java/com/pedro/taskmanagement/exception/TokenCreateException.java
+++
@@ -1,8 +0,0 @@
-package com.pedro.taskmanagement.exception;
-
-public class TokenCreateException extends RuntimeException{
-
-    public TokenCreateException(String msg) {
-        super(msg);
-    }
-}
